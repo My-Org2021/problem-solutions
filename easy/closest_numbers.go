a5fa318a6805108b0eb5c5840912320068447e56
Ismael Shakverdiev <st.bahahd@gmail.com>
2021-08-07 12:06:16 +0400

    Improve solution for Closest Numbers problem (Golang)

--- easy/closest_numbers.go
+++ easy/closest_numbers.go
@@ -15,48 +15,24 @@ func main() {
 	fmt.Println(result)
 }
 
-func closestNumbers(arr []int32) []int32 {
-	array := convertToInt(arr)
-	sort.Ints(array)
-	var result []int
+func closestNumbers(array []int32) []int32 {
+	sort.Slice(array, func(i, j int) bool { return array[i] < array[j] })
+	var result []int32
 
 	min := array[len(array)-1]
-	fmt.Println(min)
 
 	for i := 0; i < len(array)-1; i++ {
-		values := []int{array[i], array[i+1]}
-		res := int(math.Abs(float64(values[0]) - float64(values[1])))
+		values := []int32{array[i], array[i+1]}
+		res := int32(math.Abs(float64(values[0]) - float64(values[1])))
 
 		if res == min {
 			result = append(result, values...)
 		}
 		if res < min {
-			result = []int{}
-			result = append(result, values...);
-			min = res;
+			result = []int32{}
+			result = append(result, values...)
+			min = res
 		}
-	}
-
-	return convertToInt32(result)
-}
-
-// Basicaly it converts array's values --> int32 to int 
-func convertToInt(list []int32) []int {
-	result := []int{}
-
-	for i := 0; i < len(list); i++ {
-		result = append(result, int(list[i]))
-	}
-
-	return result
-}
-
-// Basicaly it converts array's values --> int to int32 
-func convertToInt32(list []int) []int32 {
-	result := []int32{}
-
-	for i := 0; i < len(list); i++ {
-		result = append(result, int32(list[i]))
 	}
 
 	return result
